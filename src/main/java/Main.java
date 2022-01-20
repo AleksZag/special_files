df2ca607c6579018b0d2cde64dda8504ed97c245
Alexander Zagrebnev <aszagrebnev@gmail.com>
2022-01-20 22:11:56 +0500

    second task completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -26,10 +27,13 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
 
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json);
+        List<Employee> listCSV = parseCSV(columnMapping, fileName);
+        String jsonCSV = listToJson(listCSV);
+        writeString(jsonCSV, "data.json");
 
+        List<Employee> listXML = parseXML("data.xml");
+        String jsonXML = listToJson(listXML);
+        writeString(jsonXML, "data2.json");
 
 
     }
@@ -52,18 +56,42 @@ public class Main {
         }
         return list;
     }
+    
+
+    public static List<Employee> parseXML(String fileName) throws IOException, SAXException, ParserConfigurationException {
+        List<Employee> list = new ArrayList<>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+
+        NodeList nodeList = doc.getElementsByTagName("employee");
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node employee = nodeList.item(i);
+            Element e = (Element) employee;
+            String id = e.getElementsByTagName("id").item(0).getTextContent();
+            String firstName = e.getElementsByTagName("firstName").item(0).getTextContent();
+            String lastName = e.getElementsByTagName("lastName").item(0).getTextContent();
+            String country = e.getElementsByTagName("country").item(0).getTextContent();
+            String age = e.getElementsByTagName("age").item(0).getTextContent();
+            list.add(new Employee(Long.parseLong(id), firstName, lastName, country, Integer.parseInt(age)));
+
+        }
+        return list;
+    }
+
 
     public static String listToJson(List<Employee> list) {
-        Type listType = new TypeToken<List<Employee>>() {
-        }.getType();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         String json = gson.toJson(list, listType);
         return json;
     }
 
-    public static void writeString(String json) {
-        try (FileWriter file = new FileWriter("data.json")) {
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
@@ -75,3 +103,4 @@ public class Main {
 
 
 }
+
